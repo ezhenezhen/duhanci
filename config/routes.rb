b5f487fb6bebfa0d25a4f0eb9492c5e444c6d864
Eugene Karpenko <ezhenezhen@gmail.com>
2013-03-28 13:01:41 +0200

    added filters, JS price slider(not working still)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Duhanci::Application.routes.draw do
   match 'products/brands' => 'products#parse_all_brands'
   match 'products/items' => 'products#parse_products_information'
-  match 'products/filter' => 'products#filter', :as => :products
+  match 'products/filter' => 'products#filter'
   resources :products
   resources :files
   match 'products/clear_filters'=> 'products#clear_filters'
